9ff831491d49016f024cb0406225918b841f3366
Freddie Brown <fred@noser.net>
2020-06-27 14:31:58 +0100

    start migration to use quinn
    This commit is the start of using Quinn so that this will be a QUIC load
    balancer.

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,68 @@
 #![warn(rust_2018_idioms)]
 
-use std::error::Error;
-use std::net::SocketAddr;
-use std::env;
-use tokio::net::UdpSocket;
-use std::str;
-use std::collections::HashMap;
+use std::{
+    ascii, fs, io,
+    net::SocketAddr,
+    path::{self, Path, PathBuf},
+    str,
+    sync::Arc,
+};
 
-mod server;
-// mod listener;
+use anyhow::{anyhow, bail, Context, Result};
+use futures::{StreamExt, TryFutureExt};
+use structopt::{self, StructOpt};
+use tracing::{error, info, info_span};
+use tracing_futures::Instrument as _;
 
+
+// From Quinn example
+#[derive(StructOpt, Debug)]
+#[structopt(name = "server")]
+struct Opt {
+    /// file to log TLS keys to for debugging
+    #[structopt(long = "keylog")]
+    keylog: bool,
+    /// directory to serve files from
+    #[structopt(parse(from_os_str))]
+    root: PathBuf,
+    /// TLS private key in PEM format
+    #[structopt(parse(from_os_str), short = "k", long = "key", requires = "cert")]
+    key: Option<PathBuf>,
+    /// TLS certificate in PEM format
+    #[structopt(parse(from_os_str), short = "c", long = "cert", requires = "key")]
+    cert: Option<PathBuf>,
+    /// Enable stateless retries
+    #[structopt(long = "stateless-retry")]
+    stateless_retry: bool,
+    /// Address to listen on
+    #[structopt(long = "listen", default_value = "[::1]:4433")]
+    listen: SocketAddr,
+}
+
+fn main() {
+
+    tracing::subscriber::set_global_default(
+        tracing_subscriber::FmtSubscriber::builder()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .finish(),
+    )
+    .unwrap();
+    let opt = Opt::from_args();
+    let code = {
+        if let Err(e) = run(opt) {
+            eprintln!("ERROR: {}", e);
+            1
+        } else {
+            0
+        }
+    };
+    println!("Hello World!");
+    ::std::process::exit(code);
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let addr = env::args()
-        .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+async fn run(options: Opt) -> Result<()>{
 
-    // Build Vec of servers
-    let mut clients = server::Server::create_from_file();
-    println!("{:?}", &clients);
-    let mut counter = 0;
-
-    // Create UDP listener for clients
-    let mut socket = UdpSocket::bind(&addr).await?;
-    println!("Listening on: {}", socket.local_addr()?);
-    let mut buf = [0; 1024];
-    let mut to_send: Option<(usize, SocketAddr)> = None; 
-    loop {  
-        // When data has been received over the socket, the packet will be dealt with
-        // Here, it will be sent onto a server connected to the 
-        if let Some((size, peer)) = to_send {
-            // Stabilize will take packet, and send it onto one of its servers. IN PROGRESS
-            println!("Data Received from {}: {}", &peer, str::from_utf8(&buf[..size])?);
-            let (client, mut val) = clients[counter];
-            val = Some(peer);
-            clients[counter] = (client, val);
-            counter += 1;
-            if counter == clients.len() {
-                counter = 0;
-            }
-        }
-        println!("{:?}", &clients);
-        // Stabilize listens for data over the socket
-        to_send = Some(socket.recv_from(&mut buf).await.unwrap());
-    }
+    Ok(())
 
 }
